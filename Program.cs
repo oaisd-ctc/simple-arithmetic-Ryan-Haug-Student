f6d0b033216d51f0ef635a2f7d2e6655cdb789a9
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-10-08 17:16:55 +0000

    made the calculator

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// used for reference
+// Console.WriteLine("");
+// int name = int.parse("string input")
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("WELCOME TO WORST CALC EVER!!!!!!!");
+
+        Console.WriteLine("Input first number: ");
+        float num1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Input second number: ");
+        float num2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please select operator | +, -, *, /, %(mod/remainder div) |");
+        string op = (Console.ReadLine());
+
+        if (op == "+")
+        {
+            Console.WriteLine("Your answer is = " + (num1 + num2));
+        }
+
+        else if (op == "-")
+        {
+            Console.WriteLine("Your answer is = " + (num1 - num2));
+        }
+
+        else if (op == "*")
+        {
+            Console.WriteLine("Your answer is = " + (num1 * num2));
+        }
+
+        else if (op == "/")
+        {
+            Console.WriteLine("Your answer is = " + (num1 / num2));
+        }
+
+                else if (op == "%")
+        {
+            Console.WriteLine("Your answer is = " + (num1 % num2));
+        }
+
+        else{
+            Console.WriteLine("Please select a valid operator");
+        }
+    }
+}
